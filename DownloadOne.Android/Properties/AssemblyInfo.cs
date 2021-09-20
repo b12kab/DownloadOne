5f16b7718fc9fe4204ab1919b482bf3cc23e4ff2
author <keith.beatty@outlook.com>
2021-09-20 13:51:38 -0400

    update version

--- DownloadOne.Android/Properties/AssemblyInfo.cs
+++ DownloadOne.Android/Properties/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using Android.App;
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Name=Android.Manifest.Permission.WriteExternalStorage, MaxSdkVersion = 29)]
+[assembly: UsesPermission(Name=Android.Manifest.Permission.WriteExternalStorage, MaxSdkVersion = 28)]
